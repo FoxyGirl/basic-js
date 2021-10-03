798c7dd5f8fc2a5eaeefd672e972d81e0c7ac23e
Elena Pavlova <lenlho22@gmail.com>
2021-10-03 22:15:19 +0300

    feat: add countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,10 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats( matrix ) {
+  const result = matrix.reduce((acc, line) => 
+    acc + line.filter(box => box === '^^').length, 
+    0);
+
+  return result;
 }
