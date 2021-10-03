56aa42be0d3728b50c89cf0cba32efd04f4d079b
Elena Pavlova <lenlho22@gmail.com>
2021-10-03 22:35:46 +0300

    feat: implement createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  const result = members.filter(item => typeof item === 'string')
+    .map(x => x.trim().toUpperCase())
+    .sort()
+    .reduce((acc, member) => acc + member[0], '');
+  
+  return result;
 }
